dd51c8943a6743af1494c0121149308e155904d3
Charles Loehle <clguitar1@gmail.com>
2023-04-21 15:44:18 -0400

    initial commit

---
+++ clweb-pagespeed.php
@@ -0,0 +1,79 @@
+<?php
+/* Plugin Name: CL Web Pagespeed
+* Plugin URI: http://cldigitaldesign.com
+* Description: Plugin for testing code for Wordpress pagespeed optimization.
+* Author: Charles Loehle
+* Version: 1.5.50
+* Author URI: http://cldigitaldesign.com
+* Text Domain: clweb-pagespeed
+* Domain Path: /languages
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+// #1 Class based
+// class CLWebPagespeed {
+// 	public function __construct() {
+// 		add_action('init', array($this, 'code_to_run_on_init'));
+// 	}
+
+// 	public function code_to_run_on_init() { 
+// 		add_action('wp', 'inner_func');
+// 		function inner_func() {
+// 			if ( is_page('about') ) {
+// 				echo '<h1>This is the about page!</h1>';
+// 			} 
+
+// 			// // Eliminate unused CSS
+// 			// function my_dequeue_script(){
+// 			// 	wp_dequeue_style('wp-block-library');	
+// 			// 	wp_dequeue_style('hello-elementor-theme-style');
+// 			// 	wp_dequeue_style('hello-elementor');
+// 			// }
+// 			// add_action('wp_enqueue_scripts', 'my_dequeue_script', 99 );			
+// 		} 
+    
+// 	}
+// }
+
+// Testing 
+// function my_test_func() {
+// 	$var = '<h1>This is the about us page!</h1>';
+
+// 	if (is_page('about-us')) {
+// 		print_r($var);
+// 		var_dump(is_page('about-us'));
+// 	}
+// }
+// add_action( 'wp','my_test_func' );
+
+// new CLWebPagespeed;
+
+// Eliminate unused CSS
+function my_dequeue_script(){
+	wp_dequeue_style('wp-block-library');	
+	wp_dequeue_style('hello-elementor-theme-style');
+	wp_dequeue_style('hello-elementor');
+}
+add_action('wp_enqueue_scripts', 'my_dequeue_script', 99 );	
+
+// First Contentful Paint (FCP) and render blocking resources
+function my_preload() {
+	?>
+		<!-- Eliminate render blocking CSS with preloading -->
+		<!-- https://web.dev/defer-non-critical-css/ -->
+		<link rel="preload" as="style" href="http://wilsonport-elementor.local/wp-content/cache/wpo-minify/1682098332/assets/wpo-minify-header-1acd49d9.min.css" onload="this.onload=null;this.rel='stylesheet'"/>
+		<link rel="preload" as="style" href="http://wilsonport-elementor.local/wp-content/plugins/unlimited-elements-for-elementor/assets_libraries/font-awesome5/css/fontawesome-all.min.css" onload="this.onload=null;this.rel='stylesheet'"/>
+		<link rel="preload" as="style" href="http://wilsonport-elementor.local/wp-content/plugins/unlimited-elements-for-elementor/assets_libraries/font-awesome5/css/fontawesome-v4-shims.css" onload="this.onload=null;this.rel='stylesheet'"/>
+		<link rel="preload" as="style" href="http://wilsonport-elementor.local/wp-content/uploads/ac_assets/uc_flip_box_base/animations/ue-flipbox-styles.css" onload="this.onload=null;this.rel='stylesheet'"/>
+		<link rel="preload" as="style" href="http://wilsonport-elementor.local/wp-content/plugins/elementor/assets/css/widget-icon-list.min.css" onload="this.onload=null;this.rel='stylesheet'"/>
+
+		<!-- First Contentful Paint - preload hero image -->
+		<!-- https://web.dev/preload-critical-assets/ -->
+		<link rel="preload" as="image" href="http://wilsonport-elementor.local/wp-content/uploads/2022/04/truck-bg.jpg"/>
+
+	<?php
+}
+add_action('wp_head', 'my_preload', 3 );
